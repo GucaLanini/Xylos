3f51410bc1be230f474855649f3085d07d447ea5
Francesco <f.dellachiara@campus.unimib.it>
2024-12-29 14:40:24 +0100

    milestone struttura

--- js/script.js
+++ js/script.js
@@ -10,163 +10,259 @@ const content = document.getElementById('content');
 fauna.addEventListener('click', () => {
   // Sostituisci l'intero contenuto del div
   content.innerHTML = `
-        <div class="row align-items-start">
+<div class="row reset-all align-items-start">
 
-            <a href="lumineth.html" class="col box areas">
+                    <div class="col box area-item">
+                        
+                        <img class="img img-item" src="tunnel.png" alt="Lumineth">
+                        <div class="overlay-item"></div>
+                        <div class="text-item">
+                            <h2 class="semibold">Lumineth</h2>
+                            <p class="text">
+                                I Nyxari sono felini, simili alle tigri terrestri, con un manto luminoso. Sono animali solitari ma curiosi. Se attratti da uno stimolo luminoso, possono avvicinarsi cautamente.
+                            </p>
+                        </div>
+                        
+        
+                    </div>
 
-                <img class="img" src="tunnel.png" alt="Lumineth">
-                <h2 class="tty box-txt tcard">Lumineth</h2>
+                    <div class="col box area-item low">
+                        
+                        <img class="img img-item" src="tunnel.png" alt="Lumineth">
+                        <div class="overlay-item"></div>
+                        <div class="text-item">
+                            <h2 class="semibold">Lumineth</h2>
+                            <p class="text">
+                                I Nyxari sono felini, simili alle tigri terrestri, con un manto luminoso. Sono animali solitari ma curiosi. Se attratti da uno stimolo luminoso, possono avvicinarsi cautamente.
+                            </p>
+                        </div>
+                        
+        
+                    </div>
 
-            </a>
-
-            <a href="" class="col box areas low">
-
-                <img class="img" src="tunnel.png" alt="Arakhara">
-                <h2 class="tty box-txt tcard">Arakhara</h2>
-
-            </a> 
-
-        </div>
-
-        <div class="row align-items-end join">
-
-            <a href="" class="col box areas low">
-
-                <img class="img" src="tunnel.png" alt="Nyxanthis">
-                <h2 class="tty box-txt tcard">Nyxanthis</h2>
-
-            </a>
-
-            <a href="" class="col box areas">
-                
-                <img class="img" src="tunnel.png" alt="Aerathos">
-                <h2 class="tty box-txt tcard">Aerathos</h2>
-                
-            </a> 
-
-        </div>
+        
+                </div>
+        
+                <div class="row reset-all join align-items-end">
+        
+                    <div class="col box area-item low">
+                        
+                        <img class="img img-item" src="tunnel.png" alt="Lumineth">
+                        <div class="overlay-item"></div>
+                        <div class="text-item">
+                            <h2 class="semibold">Lumineth</h2>
+                            <p class="text">
+                                I Nyxari sono felini, simili alle tigri terrestri, con un manto luminoso. Sono animali solitari ma curiosi. Se attratti da uno stimolo luminoso, possono avvicinarsi cautamente.
+                            </p>
+                        </div>
+                        
+        
+                    </div>
+                    <div class="col box area-item">
+                        
+                        <img class="img img-item" src="tunnel.png" alt="Lumineth">
+                        <div class="overlay-item"></div>
+                        <div class="text-item">
+                            <h2 class="semibold">Lumineth</h2>
+                            <p class="text">
+                                I Nyxari sono felini, simili alle tigri terrestri, con un manto luminoso. Sono animali solitari ma curiosi. Se attratti da uno stimolo luminoso, possono avvicinarsi cautamente.
+                            </p>
+                        </div>
+        
+                </div>
   `;
 });
 
 flora.addEventListener('click', () => {
     // Sostituisci l'intero contenuto del div
     content.innerHTML = `
-        <div class="row align-items-start">
+        <div class="row reset-all align-items-start">
 
-            <a href="lumineth.html" class="col box areas">
+                    <div class="col box area-item">
+                        
+                        <img class="img img-item" src="tunnel.png" alt="Lumineth">
+                        <div class="overlay-item"></div>
+                        <div class="text-item">
+                            <h2 class="semibold">Lumineth</h2>
+                            <p class="text">
+                                I Nyxari sono felini, simili alle tigri terrestri, con un manto luminoso. Sono animali solitari ma curiosi. Se attratti da uno stimolo luminoso, possono avvicinarsi cautamente.
+                            </p>
+                        </div>
+                        
+        
+                    </div>
 
-                <img class="img" src="tunnel.png" alt="Lumineth">
-                <h2 class="tty box-txt tcard">Lumineth</h2>
+                    <div class="col box area-item low">
+                        
+                        <img class="img img-item" src="tunnel.png" alt="Lumineth">
+                        <div class="overlay-item"></div>
+                        <div class="text-item">
+                            <h2 class="semibold">Lumineth</h2>
+                            <p class="text">
+                                I Nyxari sono felini, simili alle tigri terrestri, con un manto luminoso. Sono animali solitari ma curiosi. Se attratti da uno stimolo luminoso, possono avvicinarsi cautamente.
+                            </p>
+                        </div>
+                        
+        
+                    </div>
 
-            </a>
-
-            <a href="" class="col box areas low">
-
-                <img class="img" src="tunnel.png" alt="Arakhara">
-                <h2 class="tty box-txt tcard">Arakhara</h2>
-
-            </a> 
-
-        </div>
-
-        <div class="row align-items-end join">
-
-            <a href="" class="col box areas low">
-
-                <img class="img" src="tunnel.png" alt="Nyxanthis">
-                <h2 class="tty box-txt tcard">Nyxanthis</h2>
-
-            </a>
-
-            <a href="" class="col box areas">
-                
-                <img class="img" src="" alt="Aerathos">
-                <h2 class="tty box-txt tcard">Aerathos</h2>
-                
-            </a> 
-
-        </div>
+        
+                </div>
+        
+                <div class="row reset-all join align-items-end">
+        
+                    <div class="col box area-item low">
+                        
+                        <img class="img img-item" src="tunnel.png" alt="Lumineth">
+                        <div class="overlay-item"></div>
+                        <div class="text-item">
+                            <h2 class="semibold">Lumineth</h2>
+                            <p class="text">
+                                I Nyxari sono felini, simili alle tigri terrestri, con un manto luminoso. Sono animali solitari ma curiosi. Se attratti da uno stimolo luminoso, possono avvicinarsi cautamente.
+                            </p>
+                        </div>
+                        
+        
+                    </div>
+                    <div class="col box area-item">
+                        
+                        <img class="img img-item" src="tunnel.png" alt="Lumineth">
+                        <div class="overlay-item"></div>
+                        <div class="text-item">
+                            <h2 class="semibold">Lumineth</h2>
+                            <p class="text">
+                                I Nyxari sono felini, simili alle tigri terrestri, con un manto luminoso. Sono animali solitari ma curiosi. Se attratti da uno stimolo luminoso, possono avvicinarsi cautamente.
+                            </p>
+                        </div>
+        
+                </div>
     `;
   });
   fluviale.addEventListener('click', () => {
     // Sostituisci l'intero contenuto del div
     content.innerHTML = `
-        <div class="row align-items-start">
+        <div class="row reset-all align-items-start">
 
-            <a href="lumineth.html" class="col box areas">
+                    <div class="col box area-item">
+                        
+                        <img class="img img-item" src="tunnel.png" alt="Lumineth">
+                        <div class="overlay-item"></div>
+                        <div class="text-item">
+                            <h2 class="semibold">Lumineth</h2>
+                            <p class="text">
+                                I Nyxari sono felini, simili alle tigri terrestri, con un manto luminoso. Sono animali solitari ma curiosi. Se attratti da uno stimolo luminoso, possono avvicinarsi cautamente.
+                            </p>
+                        </div>
+                        
+        
+                    </div>
 
-                <img class="img" src="" alt="Lumineth">
-                <h2 class="tty box-txt tcard">Lumineth</h2>
+                    <div class="col box area-item low">
+                        
+                        <img class="img img-item" src="tunnel.png" alt="Lumineth">
+                        <div class="overlay-item"></div>
+                        <div class="text-item">
+                            <h2 class="semibold">Lumineth</h2>
+                            <p class="text">
+                                I Nyxari sono felini, simili alle tigri terrestri, con un manto luminoso. Sono animali solitari ma curiosi. Se attratti da uno stimolo luminoso, possono avvicinarsi cautamente.
+                            </p>
+                        </div>
+                        
+        
+                    </div>
 
-            </a>
-
-            <a href="" class="col box areas low">
-
-                <img class="img" src="tunnel.png" alt="Arakhara">
-                <h2 class="tty box-txt tcard">Arakhara</h2>
-
-            </a> 
-
-        </div>
-
-        <div class="row align-items-end join">
-
-            <a href="" class="col box areas low">
-
-                <img class="img" src="tunnel.png" alt="Nyxanthis">
-                <h2 class="tty box-txt tcard">Nyxanthis</h2>
-
-            </a>
-
-            <a href="" class="col box areas">
-                
-                <img class="img" src="tunnel.png" alt="Aerathos">
-                <h2 class="tty box-txt tcard">Aerathos</h2>
-                
-            </a> 
-
-        </div>
+        
+                </div>
+        
+                <div class="row reset-all join align-items-end">
+        
+                    <div class="col box area-item low">
+                        
+                        <img class="img img-item" src="tunnel.png" alt="Lumineth">
+                        <div class="overlay-item"></div>
+                        <div class="text-item">
+                            <h2 class="semibold">Lumineth</h2>
+                            <p class="text">
+                                I Nyxari sono felini, simili alle tigri terrestri, con un manto luminoso. Sono animali solitari ma curiosi. Se attratti da uno stimolo luminoso, possono avvicinarsi cautamente.
+                            </p>
+                        </div>
+                        
+        
+                    </div>
+                    <div class="col box area-item">
+                        
+                        <img class="img img-item" src="tunnel.png" alt="Lumineth">
+                        <div class="overlay-item"></div>
+                        <div class="text-item">
+                            <h2 class="semibold">Lumineth</h2>
+                            <p class="text">
+                                I Nyxari sono felini, simili alle tigri terrestri, con un manto luminoso. Sono animali solitari ma curiosi. Se attratti da uno stimolo luminoso, possono avvicinarsi cautamente.
+                            </p>
+                        </div>
     `;
   });
 
   rovine.addEventListener('click', () => {
     // Sostituisci l'intero contenuto del div
     content.innerHTML = `
-        <div class="row align-items-start">
+        <div class="row reset-all align-items-start">
 
-            <a href="lumineth.html" class="col box areas">
+                    <div class="col box area-item">
+                        
+                        <img class="img img-item" src="tunnel.png" alt="Lumineth">
+                        <div class="overlay-item"></div>
+                        <div class="text-item">
+                            <h2 class="semibold">Lumineth</h2>
+                            <p class="text">
+                                I Nyxari sono felini, simili alle tigri terrestri, con un manto luminoso. Sono animali solitari ma curiosi. Se attratti da uno stimolo luminoso, possono avvicinarsi cautamente.
+                            </p>
+                        </div>
+                        
+        
+                    </div>
 
-                <img class="img" src="tunnel.png" alt="Lumineth">
-                <h2 class="tty box-txt tcard">Lumineth</h2>
+                    <div class="col box area-item low">
+                        
+                        <img class="img img-item" src="tunnel.png" alt="Lumineth">
+                        <div class="overlay-item"></div>
+                        <div class="text-item">
+                            <h2 class="semibold">Lumineth</h2>
+                            <p class="text">
+                                I Nyxari sono felini, simili alle tigri terrestri, con un manto luminoso. Sono animali solitari ma curiosi. Se attratti da uno stimolo luminoso, possono avvicinarsi cautamente.
+                            </p>
+                        </div>
+                        
+        
+                    </div>
 
-            </a>
-
-            <a href="" class="col box areas low">
-
-                <img class="img" src="" alt="Arakhara">
-                <h2 class="tty box-txt tcard">Arakhara</h2>
-
-            </a> 
-
-        </div>
-
-        <div class="row align-items-end join">
-
-            <a href="" class="col box areas low">
-
-                <img class="img" src="tunnel.png" alt="Nyxanthis">
-                <h2 class="tty box-txt tcard">Nyxanthis</h2>
-
-            </a>
-
-            <a href="" class="col box areas">
-                
-                <img class="img" src="tunnel.png" alt="Aerathos">
-                <h2 class="tty box-txt tcard">Aerathos</h2>
-                
-            </a> 
-
-        </div>
+        
+                </div>
+        
+                <div class="row reset-all join align-items-end">
+        
+                    <div class="col box area-item low">
+                        
+                        <img class="img img-item" src="tunnel.png" alt="Lumineth">
+                        <div class="overlay-item"></div>
+                        <div class="text-item">
+                            <h2 class="semibold">Lumineth</h2>
+                            <p class="text">
+                                I Nyxari sono felini, simili alle tigri terrestri, con un manto luminoso. Sono animali solitari ma curiosi. Se attratti da uno stimolo luminoso, possono avvicinarsi cautamente.
+                            </p>
+                        </div>
+                        
+        
+                    </div>
+                    <div class="col box area-item">
+                        
+                        <img class="img img-item" src="tunnel.png" alt="Lumineth">
+                        <div class="overlay-item"></div>
+                        <div class="text-item">
+                            <h2 class="semibold">Lumineth</h2>
+                            <p class="text">
+                                I Nyxari sono felini, simili alle tigri terrestri, con un manto luminoso. Sono animali solitari ma curiosi. Se attratti da uno stimolo luminoso, possono avvicinarsi cautamente.
+                            </p>
+                        </div>
     `;
   });
 
